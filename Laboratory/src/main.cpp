aba995cf9451269c57e2f6f453863331b62a3b20
Lincoln Hach Yellick <lincoln.yellick@gmail.com>
2020-06-23 21:01:55 -0500

    .

--- Laboratory/src/main.cpp
+++ Laboratory/src/main.cpp
@@ -1,9 +1,39 @@
-#include <Arduino.h>
+/*
+  Moves "Boxhead" around screen like a DVD screen saver.
+*/
 
-void setup() {
-  // put your setup code here, to run once:
+#include <Arduboy2.h>
+
+Arduboy2 arduboy;
+
+int x_min, x_max, x_center, y_min, y_max, y_center;
+
+const int CHAR_WIDTH = 6;
+const int CHAR_HEIGHT = 8;
+
+String title = String("Boxhead");
+
+void setup()
+{
+  int len = title.length() * CHAR_WIDTH;
+
+  x_min = 0;
+  y_min = 0;
+
+  x_max = WIDTH - len;
+  y_max = HEIGHT - CHAR_HEIGHT;
+
+  x_center = (WIDTH - len)/2;
+  y_center = (HEIGHT - (CHAR_HEIGHT))/2;
+
+  arduboy.begin();
+  arduboy.clear();
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void loop()
+{
+  arduboy.clear();
+  arduboy.setCursor(x_center, y_center);
+  arduboy.print(title);
+  arduboy.display();
 }
