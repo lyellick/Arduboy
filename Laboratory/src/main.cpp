95bcd4d5c11d0b99346e546f750a1c795abb8417
Lincoln Hach Yellick <lincoln.yellick@gmail.com>
2020-06-25 10:00:26 -0500

    Update main.cpp

--- Laboratory/src/main.cpp
+++ Laboratory/src/main.cpp
@@ -1,39 +1,144 @@
 /*
-  Moves "Boxhead" around screen like a DVD screen saver.
+  Moves "Hello, World!" around screen like a DVD screen saver.
 */
 
 #include <Arduboy2.h>
 
 Arduboy2 arduboy;
 
-int x_min, x_max, x_center, y_min, y_max, y_center;
+enum Direction { UpLeft, UpRight, DownLeft, DownRight, Default };
+
+int x_min, x_max, x_center, x_current, y_min, y_max, y_center, y_current, speed;
+
+bool paused;
+
+Direction direction;
 
 const int CHAR_WIDTH = 6;
 const int CHAR_HEIGHT = 8;
 
-String title = String("Boxhead");
+String title = String("Hello, World!");
 
-void setup()
-{
-  int len = title.length() * CHAR_WIDTH;
+void setup() {
+    randomSeed(analogRead(0));
 
-  x_min = 0;
-  y_min = 0;
+    // title length in pixels
+    int len = title.length() * CHAR_WIDTH;
 
-  x_max = WIDTH - len;
-  y_max = HEIGHT - CHAR_HEIGHT;
+    // axis minimums
+    x_min = 0, y_min = 0;
 
-  x_center = (WIDTH - len)/2;
-  y_center = (HEIGHT - (CHAR_HEIGHT))/2;
+    // axis maximums
+    x_max = WIDTH - len, y_max = HEIGHT - CHAR_HEIGHT;
 
-  arduboy.begin();
-  arduboy.clear();
+    // axis center
+    x_center = (WIDTH - len) / 2, y_center = (HEIGHT - (CHAR_HEIGHT)) / 2;
+
+    // current cursor initialization
+    x_current = random(0, x_min), y_current = random(0, y_max);
+
+    // set inital direction
+    direction = (Direction)random(0, Default);
+
+    // set speed
+    speed = 10;
+
+    // set paused state
+    paused = false;
+
+    arduboy.begin();
+    arduboy.clear();
 }
 
-void loop()
-{
-  arduboy.clear();
-  arduboy.setCursor(x_center, y_center);
-  arduboy.print(title);
-  arduboy.display();
+void moveTitle(int speed) {
+    arduboy.clear();
+    arduboy.setCursor(x_current, y_current);
+    arduboy.print(title);
+    arduboy.display();
+    delay(speed);
+}
+
+void nextPosition() {
+    switch (direction) {
+        case UpLeft:
+            if (y_current != y_min && x_current != x_min) {
+                y_current--;
+                x_current--;
+            } else {
+                if (y_current == y_min) {
+                    direction = DownLeft;
+                }
+
+                if (x_current == x_min) {
+                    direction = UpRight;
+                }
+            }
+            break;
+        case UpRight:
+            if (y_current != y_min && x_current != x_max) {
+                y_current--;
+                x_current++;
+            } else {
+                if (y_current == y_min) {
+                    direction = DownRight;
+                }
+
+                if (x_current == x_max) {
+                    direction = UpLeft;
+                }
+            }
+            break;
+        case DownLeft:
+            if (y_current != y_max && x_current != x_min) {
+                y_current++;
+                x_current--;
+            } else {
+                if (y_current == y_max) {
+                    direction = UpLeft;
+                }
+
+                if (x_current == x_min) {
+                    direction = DownRight;
+                }
+            }
+            break;
+        case DownRight:
+            if (y_current != y_max && x_current != x_max) {
+                y_current++;
+                x_current++;
+            } else {
+                if (y_current == y_max) {
+                    direction = UpRight;
+                }
+
+                if (x_current == x_max) {
+                    direction = DownLeft;
+                }
+            }
+            break;
+        default:
+            break;
+    }
+}
+
+void loop() {
+    arduboy.pollButtons();
+    if (arduboy.justPressed(UP_BUTTON)) {
+        if (speed != 0) {
+            speed--;
+        }
+    }
+    if (arduboy.justPressed(DOWN_BUTTON)) {
+        speed += 5;
+    }
+    if (arduboy.justPressed(A_BUTTON)) {
+        speed = 10;
+    }
+    if (arduboy.justPressed(B_BUTTON)) {
+        paused = !paused;
+    }
+    if (!paused) {
+        nextPosition();
+        moveTitle(speed);
+    }
 }
