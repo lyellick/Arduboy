0db1d77a9481a634b11f5bb68982504dbbe82b7e
Lincoln Hach Yellick <lincoln.yellick@gmail.com>
2020-06-26 20:13:11 -0500

    Base input setup

--- Boxhead/src/boxhead.ino
+++ Boxhead/src/boxhead.ino
@@ -4,9 +4,39 @@ Arduboy2 arduboy;
 
 void setup() {
     arduboy.begin();
-    arduboy.setFrameRate();
+    arduboy.setFrameRate(60);
 }
 
 void loop() {
-    // put your main code here, to run repeatedly:
+    if (!arduboy.nextFrame()) {
+        return;
+    }
+
+    arduboy.pollButtons();
+
+    // Input: undefined
+    if (arduboy.justPressed(UP_BUTTON)) {
+    }
+
+    // Input: undefined
+    if (arduboy.justPressed(RIGHT_BUTTON)) {
+    }
+
+    // Input: undefined
+    if (arduboy.justPressed(DOWN_BUTTON)) {
+    }
+
+    // Input: undefined
+    if (arduboy.justPressed(LEFT_BUTTON)) {
+    }
+
+    // Input: undefined
+    if (arduboy.justPressed(A_BUTTON)) {
+    }
+
+    // Input: undefined
+    if (arduboy.justPressed(B_BUTTON)) {
+    }
+
+    arduboy.display(CLEAR_BUFFER);
 }
