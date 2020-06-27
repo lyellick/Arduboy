9364694e060184dca525286b94029fea20e9f633
Lincoln Hach Yellick <lincoln.yellick@gmail.com>
2020-06-26 20:00:36 -0500

    Boxhead Project Creation

---
+++ Boxhead/src/boxhead.ino
@@ -0,0 +1,12 @@
+#include <Arduboy2.h>
+
+Arduboy2 arduboy;
+
+void setup() {
+    arduboy.begin();
+    arduboy.setFrameRate();
+}
+
+void loop() {
+    // put your main code here, to run repeatedly:
+}
