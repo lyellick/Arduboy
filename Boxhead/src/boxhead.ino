bc5a9ec559ab816af22c6c53a74e6b1ab6043027
Lincoln Hach Yellick <lincoln.yellick@gmail.com>
2020-06-27 11:36:32 -0500

    Multiplier testing

--- Boxhead/src/boxhead.ino
+++ Boxhead/src/boxhead.ino
@@ -3,22 +3,19 @@
  * Version 1:
  *    - He lives till the player closes the game.
  *    - Score is based off the time you keep Mr. Boxhead alive.
- *    -
+ *    - Achievements per time period shown as  QR Code:
+ *        - "First Second!" - You made it past the first second!
+ *    =
  */
 
 #include <Arduboy2.h>
+#include <score.h>
 
 Arduboy2 arduboy;
-
-int8_t frames = 0;
-int8_t milliseconds = 0;
-int8_t seconds = 0;
-int8_t minutes = 0;
-int8_t hours = 0;
-int16_t days = 0;
-int32_t years = 0;
+Score score;
 
 void setup() {
+    score = Score();
     arduboy.begin();
     arduboy.setFrameRate(60);
 }
@@ -27,6 +24,8 @@ void loop() {
     if (!arduboy.nextFrame()) {
         return;
     }
+
+    score.trackGameTime();
 
     arduboy.pollButtons();
 
@@ -54,38 +53,20 @@ void loop() {
     if (arduboy.justPressed(B_BUTTON)) {
     }
 
-    ++frames;
-    if (frames == 60) {
-        frames = 0;
-        ++seconds;
-        if (seconds == 60) {
-            seconds = 0;
-            ++minutes;
-            if (minutes == 60) {
-                minutes = 0;
-                ++hours;
-                if (hours == 24) {
-                    hours = 0;
-                    ++days;
-                    if (days == 365) {
-                        days = 0;
-                        ++years;
-                    }
-                }
-            }
-        }
-    }
-
     arduboy.setCursor(0, 0);
-    arduboy.print(F("Seconds: "));
-    arduboy.println(seconds);
-    arduboy.print(F("Minutes: "));
-    arduboy.println(minutes);
-    arduboy.print(F("Hours: "));
-    arduboy.println(hours);
-    arduboy.print(F("Days: "));
-    arduboy.println(days);
-    arduboy.print(F("Years: "));
-    arduboy.println(years);
+    arduboy.print(F("Seconds:    "));
+    arduboy.println(score.seconds);
+    arduboy.print(F("Minutes:    "));
+    arduboy.println(score.minutes);
+    arduboy.print(F("Hours:      "));
+    arduboy.println(score.hours);
+    arduboy.print(F("Days:       "));
+    arduboy.println(score.days);
+    arduboy.print(F("Years:      "));
+    arduboy.println(score.years);
+    arduboy.print(F("Score:      "));
+    arduboy.println(score.getScore(), 3);
+    arduboy.print(F("Multiplier: "));
+    arduboy.println(score.getMultiplier(), 3);
     arduboy.display(CLEAR_BUFFER);
 }
