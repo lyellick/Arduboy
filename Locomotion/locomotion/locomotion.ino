ba0b87eafcca302f4076e1928e174c14641dbf74
Lincoln Hach Yellick <lincoln.yellick@gmail.com>
2020-07-10 21:05:32 -0500

    Forgot to add to run folder

--- Locomotion/locomotion/locomotion.ino
+++ Locomotion/locomotion/locomotion.ino
@@ -1,17 +1,22 @@
-/* Locomotion
+/* Locomotion: Playing around with moving an entity around a bitmap.
  *
- * Playing around with moving an entity around a bitmap.
+ * Version 1:
+ *      - Kinda fun to play a chiptune version of The Locomotion by Little Eva
+ *      - Layer One: background
+ *      - Layer Two: train sprite
  */
 
 #include <Arduboy2.h>
 
 #include "game.h"
+#include "sprites.h"
 
 Arduboy2 arduboy;
 
 Game game = Game(arduboy);
 
 constexpr int8_t frameRate = 60;
+int8_t x = 0;
 
 void setup() {
     arduboy.begin();
@@ -29,8 +34,9 @@ void loop() {
     if (arduboy.justPressed(UP_BUTTON)) {
     }
 
-    // Input: undefined
+    // Input: move right
     if (arduboy.justPressed(RIGHT_BUTTON)) {
+        x = x + 4;
     }
 
     // Input: undefined
@@ -47,6 +53,13 @@ void loop() {
 
     // Input: undefined
     if (arduboy.justPressed(B_BUTTON)) {
+    }
+
+    arduboy.drawSlowXYBitmap(0, 0, background, WIDTH, HEIGHT, WHITE);
+    if (x % 8 == 0) {
+        arduboy.drawSlowXYBitmap(x, 25, locomotionPositionOne, 48, 32, WHITE);
+    } else {
+        arduboy.drawSlowXYBitmap(x, 25, locomotionPositionTwo, 48, 32, WHITE);
     }
 
     arduboy.display(CLEAR_BUFFER);
