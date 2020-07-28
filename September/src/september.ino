eb4862c9ae4368b58a3ab39c0d31d24927098162
Lincoln Hach Yellick <lincoln.yellick@gmail.com>
2020-07-27 19:10:00 -0500

    Applied personal template

---
+++ September/src/september.ino
@@ -0,0 +1,56 @@
+/* September: A simple memory game where the more you play the faster it goes.
+ *
+ * Version 1:
+ *      - Title Screen Image
+ *      - Title Screen Ditty: September by Earth, Wind & Fire
+ *      - Class: Game.h/Game.cpp
+ */
+
+#include <Arduboy2.h>
+
+#include "game.h"
+
+Arduboy2 arduboy;
+
+Game game = Game(arduboy);
+
+constexpr int8_t frameRate = 60;
+
+void setup() {
+    arduboy.begin();
+    arduboy.setFrameRate(frameRate);
+}
+
+void loop() {
+    if (!arduboy.nextFrame()) {
+        return;
+    }
+
+    arduboy.pollButtons();
+
+    // Input: undefined
+    if (arduboy.justPressed(UP_BUTTON)) {
+    }
+
+    // Input: undefined
+    if (arduboy.justPressed(RIGHT_BUTTON)) {
+    }
+
+    // Input: undefined
+    if (arduboy.justPressed(DOWN_BUTTON)) {
+    }
+
+    // Input: undefined
+    if (arduboy.justPressed(LEFT_BUTTON)) {
+    }
+
+    // Input: undefined
+    if (arduboy.justPressed(A_BUTTON)) {
+    }
+
+    // Input: undefined
+    if (arduboy.justPressed(B_BUTTON)) {
+    }
+
+    arduboy.display(CLEAR_BUFFER);
+}
